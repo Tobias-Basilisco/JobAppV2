b9ba85a274c2e2c7f653ea01743ecd5e4adea9d8
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2023-10-06 15:42:54 +0200

    wks [id]

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,74 +1,55 @@
-import {Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl} from "react-native"
-import { Stack, useRouter, useGlobalSearchParams } from "expo-router";
-import { useCallback, useState } from "react";
+import React, { useState } from "react";
+import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from "react-native";
+import { StackActions, useNavigation, useRoute } from "@react-navigation/native"; // Import from @react-navigation/native
 
-import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics} from "../../components";
+import { Company, JobTabs, ScreenHeaderBtn } from "../../components";
 import { COLORS, SIZES, icons } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
-const jobDetails = () => {
-    const params = useGlobalSearchParams();
-    const router = useRouter();
-    const { data, isLoading, error, refetch } = useFetch('job-details', {job_id: params.id});
-    const [refreshing, setRefreshing] = useState(false);
-    const onRefresh = () => {};
-        
+const JobDetails = () => {
+  const route = useRoute(); // Use useRoute from @react-navigation/native
+  const navigation = useNavigation(); // Use useNavigation to navigate back
 
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-            <Stack.Screen
-                options={{
-                    headerStyle: { backgroundColor: COLORS.lightWhite },
-                    headerShadowVisible: false,
-                    headerBackVisible: false,
-                    headerLeft: () => (
-                        <ScreenHeaderBtn 
-                            iconUrl = {icons.left}
-                            dimension = '60%'
-                            handlePress={() => router.back()}
-                        />
-                    ),
-                    headerRight: () => (
-                        <ScreenHeaderBtn 
-                            iconUrl = {icons.share}
-                            dimension = '60%'
-                        />
-                    ),
-                    headerTitle: ""
+  const id = route.params.id.slice(1, -1); // Remove the first and last character (square brackets)
 
-                }}
+  const { data, isLoading, error, refetch } = useFetch('job-details', { job_id: id }); // Access the id parameter from route.params
+
+// console.log("Data:", data);
+// console.log("Is Loading:", isLoading);
+// console.log("Error:", error);
+// console.log('id:', route.params.id[0]);
+console.log('id:', id);
+
+
+  const [refreshing, setRefreshing] = useState(false);
+  const onRefresh = () => {
+    // Implement your refresh logic here
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : data.length === 0 ? (
+          <Text>No data</Text>
+        ) : (
+          <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+            <Company
+              companyLogo={data[0].employer_logo}
+              jobtitle={data[0].job_title}
+              companyName={data[0].employer_name}
+              location={data[0].job_country}
             />
 
-            <>
-                <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh = {onRefresh()}/>}>
-                    {isLoading ? (
-                        <ActivityIndicator size = "large" colors={COLORS.primary}/>
-                    ) : error ? (
-                        <Text>Something went wrong</Text>
-                    ) : data.length === 0 ? (
-                        <Text>No data</Text>
-                    ) : (
-                        <View style={{ padding:SIZES.medium, paddingBottom: 100 }}>
-                            <Company
-                                companyLogo={data[0].employer_logo}
-                                jobtitle={data[0].job_title}
-                                companyName={data[0].employer_name}
-                                location={data[0].job_country}
+            <JobTabs />
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-                            />
-
-                            <JobTabs
-                            
-                            />
-
-                        </View>
-                    )
-                }
-                </ScrollView>
-            </>
-
-        </SafeAreaView>
-    )
-}
-
-export default jobDetails
+export default JobDetails;
