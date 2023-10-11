2c789df804d8ba3ebf07f584c54f59571081d97f
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2023-10-11 17:03:40 +0200

    2 key

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,17 +1,28 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+import { API_KEYS } from '@env';
 
 
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const APIKeys = ['1275e6d03fmsh976127f77d23ccap154125jsnd8e5c35799fc',
+    'b781425ad4mshb3e4c64a0aa8807p12b3fajsn93e405a04ae2',
+
+    ];
+    const apiKeys = API_KEYS.split(',');
+
+    const XRapidAPIKey = apiKeys[Math.floor(Math.random() * apiKeys.length)];
+    // console.log('key: ' +XRapidAPIKey);
+    console.log('key : ' + XRapidAPIKey);
+
 
     const options = {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            'X-RapidAPI-Key': '1275e6d03fmsh976127f77d23ccap154125jsnd8e5c35799fc',
+            'X-RapidAPI-Key': XRapidAPIKey,
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
         params: { ... query },
