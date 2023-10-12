36b62b1c50b61bbe22ae7d639f08e84b22c849e6
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2023-10-12 19:32:51 +0200

    .emv

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -7,16 +7,11 @@ const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-    const APIKeys = ['1275e6d03fmsh976127f77d23ccap154125jsnd8e5c35799fc',
-    'b781425ad4mshb3e4c64a0aa8807p12b3fajsn93e405a04ae2',
-
-    ];
+    
     const apiKeys = API_KEYS.split(',');
 
     const XRapidAPIKey = apiKeys[Math.floor(Math.random() * apiKeys.length)];
-    // console.log('key: ' +XRapidAPIKey);
-    console.log('key : ' + XRapidAPIKey);
-
+    
 
     const options = {
         method: 'GET',
